6476c16011d8bd8c1ef15600d05858fbf65acac6
CorellCB <corellcb@gmail.com>
2019-11-14 14:12:32 -0500

    completed tasks 1, 2, and 3

--- index.js
+++ index.js
@@ -41,7 +41,22 @@ class Airplane {
 */
 
 class Person {
-
+  constructor(name, age) {
+    this.name = name,
+    this.age = age,
+    this.stomach = []
+  }
+  eat(edible) {
+    if (this.stomach.length < 10) {
+      this.stomach.push(edible);
+    }
+  }
+  poop(){
+    this.stomach = [];
+  }
+  toString(){
+    return `${this.name}, ${this.age}`;
+  }
 }
 
 /*
@@ -59,7 +74,28 @@ class Person {
 */
 
 class Car {
+  constructor(model, milesPerGallon) {
+    this.model = model,
+    this.milesPerGallon = milesPerGallon,
+    this.tank = 0,
+    this.odometer = 0
+  }
+  fill(gallons){
+    this.tank += gallons;
+  }
+  drive(distance){
+    let maxDistance = this.milesPerGallon * this.tank;
 
+    if(maxDistance > distance){
+      this.tank -= distance / this.milesPerGallon;
+      this.odometer += distance;
+    }
+    else if(maxDistance < distance) {
+      this.tank = 0;
+      this.odometer += maxDistance;
+      return `I ran out of fuel at ${this.odometer} miles!`;
+    }
+  }
 }
 
 /*
@@ -75,7 +111,14 @@ class Car {
         + {name} and {location} of course come from the instance's own properties.
 */
 class Lambdasian {
-
+  constructor(attributes){
+    this.name = attributes.name,
+    this.age = attributes.age,
+    this.location = attributes.location
+  }
+  speak(){
+    return `Hello my name is ${this.name}, I am from ${this.location}`;
+  }
 }
 
 /*
